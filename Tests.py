7222a0137e0ef2286fadb4a7504449ff2f4ab975
Sarah Juricic <sarah.juricic@univ-smb.fr>
2017-08-24 14:55:06 +0200

    Separate an Ishigami sampler from the exact values
    Create two separate functions : a sampler and the exact solutions

--- Tests.py
+++ Tests.py
@@ -16,14 +16,13 @@ from random import randint as randint
 #==============================================================================
 #                      Test function : ISHIGAMI
 #==============================================================================
-def ishigami_function(n):
+def ishigami_sampler(n):
     """
     ISHIGAMI function
     
     n : integer, sample size
     
     return  x,y input and output samples of the Ishigami function (n samples)
-            exactDiag, array, exact values of the sensitivity values
             
     Crestaux et al. (2007) and Marrel et al. (2009) use: a = 7 and b = 0.1.
     Sobol' & Levitan (1999) use: a = 7 and b = 0.05.
@@ -34,9 +33,18 @@ def ishigami_function(n):
     
     #generate input and output samples
     x = -pi + 2*pi*np.random.rand(n,3)
-    y = f(X).reshape((ishigami(x).shape[0],ishigami(x)[0].size))
+    y = f(x).reshape((ishigami(x).shape[0],ishigami(x)[0].size))
+    return x, y
+
+def exact_ishigami():
+    """
+    return a matrix of the exact sensitivity values
+    Crestaux et al. (2007) and Marrel et al. (2009) use: a = 7 and b = 0.1.
+    Sobol' & Levitan (1999) use: a = 7 and b = 0.05.
+    """
+    a = 7
+    b = 0.05
     
-    #exact sensitivity values
     Vx1 = 1/2*(1 + b*(pi**4)/5)**2
     Vx2 = a**2/8
     Vx13 = 8*b**2*pi**8/225
@@ -44,10 +52,9 @@ def ishigami_function(n):
     exact = np.array([[Vx1/V, 0, 0],
                        [0, Vx2/V, 0],
                        [Vx13/V, 0, 0]])
-    exactDiag = exact.diagonal()
+    return exact
     
-    return x, y, exactDiag
-
+    
 #==============================================================================
 #                       testing algorithm configurations
 #==============================================================================
@@ -66,14 +73,11 @@ def bias_effect():
     ninput = 3 # def: X=[x1,x2,x3] -> xi=U(-pi,pi)
     SIc = np.zeros((ninput,450))
     SI = np.zeros((ninput,450))
-    f,exactDiag = ishigami_function()
+    exact = exact_ishigami().diagonal()
     
     #from 50 to 500 simulations
     for N in range(50,500):
-        #create random model inputs of N simulations
-        X = -pi + 2*pi*np.random.rand(N,ninput)
-        #calculate the corresponding Ishigami function output
-        Y = f(X).reshape((f(X).shape[0],f(X)[0].size))
+        X,Y = ishigami_sampler(N)
         #calculate the corresponding sensitivity analysis...
         tSI,tSIc = rbdfast(Y, x = X, warning_on = warning_on)
         #... and reshape the results into plottable array
@@ -82,7 +86,7 @@ def bias_effect():
     # Print plot : effect of bias
     plt.plot(SI.transpose(),'r--')
     plt.plot(SIc.transpose(),'b--')
-    plt.plot([[exactDiag.item(i) for i in range(0,3)] for k in range(0,450)],
+    plt.plot([[exact.item(i) for i in range(0,3)] for k in range(0,450)],
                color = '#003366',
                linewidth = 2.0)
     plt.title('Effect of bias')
