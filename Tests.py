97bc5ef3b33ff0c099515bf00309982e20eec4af
Sarah Juricic <sarah.juricic@univ-smb.fr>
2017-08-24 18:17:15 +0200

    Add function to visualize convergence

--- Tests.py
+++ Tests.py
@@ -67,8 +67,6 @@ def bias_effect():
         to do : plot comparison for any case studies
     returns a plot of the evolution of the results depending on the simulation number
     """
-    from math import pi as pi
-    from random import randint as randint
     warning_on = False
     ninput = 3 # def: X=[x1,x2,x3] -> xi=U(-pi,pi)
     SIc = np.zeros((ninput,450))
@@ -104,23 +102,48 @@ def effect_threshold():
         To do : make any sensitivity analysis result plottable
     returns plot of the impact of changing this threshold value
     """
-    f,exactDiag = ishigami_function()
+    exact = exact_ishigami().diagonal()
     ninput = 3
     SIc = np.zeros((ninput,30))
     SI = np.zeros((ninput,30))
-    x = -pi + 2*pi*np.random.rand(500,ninput)
-    y = f(x).reshape((f(x).shape[0],f(x)[0].size))
+    x,y = ishigami_sampler(500)
+    
     for m in range(1,31):
         tempSI,tempSIc = rbdfast(y, x = x, m = m)
         SI[:,m-1],SIc[:,m-1] = tempSI.reshape((1,3)),tempSIc.reshape((1,3))
     
+    #plot the evolution of the sensitivity indices wrt threshold
     plt.plot(SIc.transpose(),'b')
     plt.plot(SI.transpose(),'r')
-    plt.plot([[exactDiag[i] for i in range(0,3)] for k in range(0,30)],'k')
+    plt.plot([[exact[i] for i in range(0,3)] for k in range(0,30)],'k')
     plt.title('Effect of the M value')
     plt.ylabel('SI')
     plt.xlabel('M value')
     plt.show()
     return
 
-
+#======================== Convergence ===============================
+def plot_convergence(x,y):
+    """
+    Visualize the convergence of the sensitivity indices
+    takes two arguments : x,y input and model output samples
+    return plot of sensitivity indices wrt number of samples
+    """
+    ninput = x.shape[1]
+    noutput = y.shape[1]
+    nsamples = x.shape[0]
+    trials = (nsamples-30)//10
+    all_si_c = np.zeros((trials,ninput,noutput))
+    for i in range(30,nsamples,10):
+        all_si_c[(i-30)//10,:] = rbdfast(y[:i],x=x[:i,:])[1]
+    
+    for i in range(ninput):
+        plt.plot(all_si_c[:,i],'k-')
+    plt.show()
+    return
+    
+    
+    
+    
+    
+    
