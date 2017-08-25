05193f9d52e7b1a4353b709d0a2d680719f0ed33
Sarah Juricic <sarah.juricic@univ-smb.fr>
2017-08-25 09:27:10 +0200

    Seperate core rbd analysis from rbd or bootstrap launchers
    Create a rbdfast_core function for the core analysis
    Separate bootstrap from rbdfast, both calling rdbfast_core

--- rbd_fast.py
+++ rbd_fast.py
@@ -6,7 +6,12 @@ Created on Wed Jun  7 14:53:15 2017
 """
 
 import numpy as np
-warning_on = False
+#warning_on = False
+
+
+#==============================================================================
+#                           Secondary functions
+#==============================================================================
 
 
 def to_integer(m):
@@ -41,40 +46,80 @@ def get_sorting_permutation(array):
 
     return perm
 
-
-def bootstrap(x, y):
+#==============================================================================
+#                           RBD FAST core analysis
+#==============================================================================
+def __rbdfast_core(x,y,n,m,k,l,useindex = False):
     """
-    Bootstrap function to evaluate sensitivity of RBD-Fast estimates
+    x, array : model input samples or index
+    y, array : model output samples
+    n : number of samples
+    m : threshold (variance calculated from the m first frequencies from FFT)
+    k : number of model inputs
+    l : number of model outputs
+    useindex : True if an index instead of model inputs is used for x
+    """
+    si = np.zeros((k, y[0].size))
+    si_c = np.zeros((k, y[0].size))
+    lamda = (2 * m) / n
+    y_org = np.zeros((y.shape[0], y[0].size))
+    
+    # for every model input in x (or pre-filled Index)
+    for col in range(k):
+        if useindex:
+            # ---- reordering of y wrt a-th index
+            for k, ind in enumerate(index[:, col]):
+                y_org[k, :] = y[ind, :].copy()
+        else:
+            # ---- reordering of y wrt a-th variable of x
+            permut = get_sorting_permutation(x[:, col])
+            # copy ordered Y into new Yorg variable
+            for i, ind in enumerate(permut):
+                y_org[i, :] = y[ind, :].copy()
+
+        # calculate spectrum with RFFT : (abs(fft(Yorg)))^2/N/(N-1)
+        # Normalization by N-1 to match the definition of the unbiased variance
+        spectrum = np.abs(np.fft.rfft(y_org[:, 0]))**2 / n / (n - 1)
+        v = sum(spectrum[1:])
+        si[col, :] = sum(spectrum[1:m + 1]) / v
+        si_c[col, :] = si[col, :] - lamda / (1 - lamda) * (1 - si[col, :])
+    return si,si_c
+
+#==============================================================================
+#                           BOOTSTRAP launcher
+#==============================================================================
+def bootstrap_c(x, y):
+    """
+    Bootstrap function to evaluate sensitivity of RBD-Fast unbiased estimates
     1000 sample
     Takes as arguments :
     x model inputs
     y model outputs
     return mean, var
     """
-    ninput = x.shape[1]
-    noutput = y.shape[1]
-    sample_size = y.shape[0]
-    all_si_c = np.empty((1000, ninput, noutput))
+    k = x.shape[1]
+    l = y.shape[1]
+    n = y.shape[0]
+    all_si_c = np.empty((1000, k, l))
 
     # calculate sensitivity coef 1000 times
     for i in range(0, 1000):
-        # random sample on sample_size n with replacement
-        indices = np.random.randint(0, high=sample_size, size=sample_size)
-        x_new = np.empty((sample_size, ninput))
-        y_new = np.empty((sample_size, noutput))
+        # random sample on sample size n with replacement
+        indices = np.random.randint(0, high=n, size=n)
+        x_new = np.empty((n, k))
+        y_new = np.empty((n, l))
         for j, index in enumerate(indices):
             x_new[j, :] = x[index, :]
             y_new[j, :] = y[index, :]
-        all_si_c[i] = rbdfast(y_new, x_new)[1]
+        all_si_c[i] = __rbdfast_core(x_new,y_new,n,10,k,l)[1]
 
     return np.mean(all_si_c, axis=0), np.var(all_si_c, axis=0)
 
 
 #==============================================================================
-#                               RBD FAST
+#                           RBD FAST launcher
 #==============================================================================
-def rbdfast(y, x=np.array([]), index=np.array([]),
-            m=10, bootstrap_on=False, warning_on=True):
+def rbdfast(y, x=np.array([]), index=np.array([]), m=10, warning_on=True):
     """
     RBD python Code for Random Balance Designs
     For the estimation of first order indices
@@ -111,74 +156,21 @@ def rbdfast(y, x=np.array([]), index=np.array([]),
             # TO DO raise proper error
             print('Error : An index of permutation must be defined.')
             return
-        n, k = index.shape[0], index[0].size
+        n, k, l = index.shape[0], index[0].size, y[0].size
         useindex = True
     else:
-        n, k = x.shape[0], x[0].size
+        n, k, l = x.shape[0], x[0].size, y[0].size
         useindex = False
 
     if y.shape[0] != n:
         print('Error : Arguments dimensions are not consistent')
         return
 
-    if n < 2 * (m + 100):
-        warning_on = True or warning_on
-        
-    #optionnal bootstrap analysis
-    if bootstrap_on:
-        return bootstrap(x,y)
-
-    # Initialization of SI and SIc matrices (sensitivity indices)
-    si = np.zeros((k, y[0].size))
-    si_c = np.zeros((k, y[0].size))
-    lamda = (2 * m) / n
-    y_org = np.zeros((y.shape[0], y[0].size))
-    # for every model input in x (or pre-filled Index)
-    for col in range(0, k):
-        if useindex:
-            # ---- reordering of y wrt a-th index
-            for k, ind in enumerate(index[:, col]):
-                y_org[k, :] = y[ind, :].copy()
-        else:
-            # ---- reordering of y wrt a-th variable of x
-            permut = get_sorting_permutation(x[:, col])
-            # copy ordered Y into new Yorg variable
-            for i, ind in enumerate(permut):
-                y_org[i, :] = y[ind, :].copy()
-
-        # calculate spectrum with RFFT : (abs(fft(Yorg)))^2/N/(N-1)
-        # Normalization by N-1 to match the definition of the unbiased variance
-        spectrum = np.abs(np.fft.rfft(y_org[:, 0]))**2 / n / (n - 1)
-        v = sum(spectrum[1:])
-        si[col, :] = sum(spectrum[1:m + 1]) / v
-        si_c[col, :] = si[col, :] - lamda / (1 - lamda) * (1 - si[col, :])
-
-#        if warning_on:
-#            print('\n~~~!!!~~~\n',
-#                  'There has been at least on low Sample Size\n',
-#                  'Insufficient simulations for proper analysis\n')
-    return si, si_c
+    if n < 2 * (m + 100) and warning_on:
+        print('WARNING : sample size insufficient for proper analysis')
+    
+    return __rbdfast_core(x,y,n,m,k,l,useindex = useindex)
+    
 
 
 
-#==============================================================================
-#           Bootstrap indicator
-#==============================================================================
-"""
-????????
-
-if isfield(analyse,'bootstrap') &&  analyse.bootstrap
-	fprintf('-> Bootstrap analysis.\n')
-	for rep = 1:analyse.bootstrap_param.rep
-		# Selection aléatoire de données avec retirage
-		Ind = randi(nbs_sorties,analyse.bootstrap_param.ech,1);
-
-		if params.type_ech==3
-			SIbs_rbdfast(:,:,rep)=rbd_fast(1,true,analyse.RBD.harmonics,params.index_rbd_fast(Ind,:),resultat.sorties(Ind,:));
-		else
-			SIbs_rbdfast(:,:,rep)=rbd_fast(1,true,analyse.RBD.harmonics,[],resultat.sorties(Ind,:),params.plan(Ind,1:params.variables.vars_nb));
-
-	analyse.SIbs_rbdfast_mean = mean(SIbs_rbdfast,3);
-	analyse.SIbs_rbdfast_var = var(SIbs_rbdfast,0,3);
-end
-"""
